cbd6b3b7a3b888ec2376d47849649be650961a68
mohamed-1998-coder <mohamed.khalladi98@gmail.com>
2020-09-15 13:44:40 +0100

    exercice

--- mohammed.js
+++ mohammed.js
@@ -1,1 +1,8 @@
 console.log("Hello! I am Mohamed");
+const tab = ['iskander','nadin','mohammed','samer','najla','marwa'] ;
+console.log(tab.map(v => v+' abbasi'));
+console.log(tab.map(v => v+' yaakoubi'));
+console.log(tab.map(v => v+' khalladi'));
+console.log(tab.map(v => v+' chouchen'));
+console.log(tab.map(v => v+' gharbi'));
+console.log(tab.map(v => v+' dhawadi'));
